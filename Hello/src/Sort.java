c508420baba60e1278a5ee332ecdd7404150fed6
Daniel Xun <1084436629@qq.com>
2018-11-10 18:36:50 +0800

    冒泡、选择与直接插入排序

---
+++ Hello/src/Sort.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] a = {45, 12, 78, 97, 10};
+        int len = a.length;
+
+        //冒泡排序
+//        for (int i = 0; i < len; i++) {
+//            for (int j = i+1; j < len; j++) {
+//                if (a[i] > a[j]) {
+//                    int temp = a[i];
+//                    a[i] = a[j];
+//                    a[j] = temp;
+//                }
+//            }
+//        }
+
+        //选择排序
+//        for (int i = 0; i < len; i++) {
+//            int min = i;
+//            for (int j = i+1; j < len; j++) {
+//                if (a[min] > a[j]) {
+//                    min = j;
+//                }
+//            }
+//            if (min != i) {
+//                int temp = a[i];
+//                a[i] = a[min];
+//                a[min] = temp;
+//            }
+//        }
+
+
+        //直接插入排序
+//        for (int i = 1; i < len; i++){
+//            int temp = a[i];
+//            int j;
+//            for (j = i-1; j >= 0; j--){
+//                if (a[j] > temp){
+//                    a[j+1] = a[j];
+//                } else {
+//                    break;
+//                }
+//            }
+//            a[j+1] = temp;
+//        }
+
+
+        System.out.println(Arrays.toString(a));
+    }
+}
