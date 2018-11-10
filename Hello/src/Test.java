8c301713e72e2e56332480e85c5edce077a5e40e
Daniel Xun <1084436629@qq.com>
2018-11-10 15:32:21 +0800

    直角三角形、等腰三角形与九九乘法表

---
+++ Hello/src/Test.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + i*j + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= 1; j--) {
+                if (j <= i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
